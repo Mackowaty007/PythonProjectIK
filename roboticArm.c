1f6feed6154896956a6cfe658dd535b0545c9fb6
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2022-05-25 18:45:30 +0200

    Add files via upload

--- roboticArm.c
+++ roboticArm.c
@@ -6,18 +6,20 @@ Servo myservo2;
 Servo myservo3;
 Servo myservo4;
 
-int pos = 45;
 int positions[SERVO_NUMBER];
 int maxPos[SERVO_NUMBER] = {180,180,180,180};
 int minPos[SERVO_NUMBER] = {0,0,0,0};
 
-bool isServoGoingUp = true;
+int data1 = 90;
+int data2 = 90;
+int data3 = 90;
+int data4 = 90;
 
 void setTheServos(){
-  myservo.write(180-positions[0]);
-  myservo2.write(180-positions[1]);
-  myservo3.write(180-positions[2]);
-  myservo4.write(180-positions[3]);
+  myservo.write(positions[0]);
+  myservo2.write(positions[1]);
+  myservo3.write(positions[2]);
+  myservo4.write(positions[3]);
 }
 
 void setup() {
@@ -26,18 +28,28 @@ void setup() {
   myservo2.attach(6);
   myservo3.attach(5);
   myservo4.attach(3);
-
-  for(int i=0;i<SERVO_NUMBER;i++){
-    positions[i] = pos;
-  }
 }
 
 void loop() {
-  pos = 90;
-  //set positions
-  for(int i=0;i<SERVO_NUMBER;i++){
-    positions[i] = pos;
+  if(Serial.available() > 0) {
+    data1 = (Serial.readStringUntil('\n')).toInt();
+    data2 = (Serial.readStringUntil('\n')).toInt();
+    data3 = (Serial.readStringUntil('\n')).toInt();
+    data4 = (Serial.readStringUntil('\n')).toInt();
+    Serial.println("got the data");
   }
+
+  
+  //set positions
+  //first servo
+  positions[2] = data1+25;
+  //second servo
+  positions[1] = data2;
+  //third servo
+  positions[0] = data3+15;
+  //base servo
+  positions[3] = 180-data4;
+  
   //check if pos aint too big or small
   for(int i=0;i<SERVO_NUMBER;i++){
     positions[i] = constrain(positions[i],minPos[i],maxPos[i]);
